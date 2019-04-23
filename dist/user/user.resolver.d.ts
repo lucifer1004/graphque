95696bc2b08b5ce8e3cb68b525f054ef441cae78
Gabriel Wu <qqbbnease1004@126.com>
2019-04-23 22:50:55 +0800

    chore: remove dist

--- dist/user/user.resolver.d.ts
+++
@@ -1,7 +0,0 @@
-import {User} from './user.entity'
-import {UserService} from './user.service'
-export declare class UserResolver {
-  private readonly userService
-  constructor(userService: UserService)
-  user(login: string): Promise<User>
-}
--- dist/user/user.service.d.ts
+++
@@ -1,7 +0,0 @@
-import {HttpService} from '@nestjs/common'
-import {User} from './user.entity'
-export declare class UserService {
-  private readonly httpService
-  constructor(httpService: HttpService)
-  getUserByLogin(login: any): Promise<User>
-}
